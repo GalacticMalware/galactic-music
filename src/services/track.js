96002e2b64d60fb67183757cf25a4ada96999140
Daniel <daniel_mg_97@hotmail.com>
2020-11-10 16:10:29 -0600

    Nuevos componentes

--- src/services/track.js
+++ src/services/track.js
@@ -8,7 +8,7 @@ trackService.search = function (q) {
     return galacticMusicService.get('/search',{
         params:{ q,type,},
         headers: {
-            "x-rapidapi-key": "98fec081dfmsh49ad33f660f88efp169e9ejsn11ec6fe46fc1",
+            "x-rapidapi-key": "",
             "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
         } 
     }).then(res=>res.data)
